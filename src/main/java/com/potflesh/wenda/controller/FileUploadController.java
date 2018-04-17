0b45d9e7e627e2cfba4161632e8d46ff4b013b45
zhangyuwei <570146800@qq.com>
2018-04-17 14:44:19 +0800

    feat(uploadFile): add uploadImage Function

--- src/main/java/com/potflesh/wenda/controller/FileUploadController.java
+++ src/main/java/com/potflesh/wenda/controller/FileUploadController.java
@@ -1,42 +1,37 @@
 package com.potflesh.wenda.controller;
-
+import com.potflesh.wenda.storage.StorageFileNotFoundException;
+import com.potflesh.wenda.storage.StorageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Created by bazinga on 11/04/2018.
  */
 @Controller
-public class UtilController {
+public class FileUploadController {
 
     @RequestMapping(value = "api/uploadImage", method = {RequestMethod.POST,RequestMethod.GET})
     String uploadImage(RedirectAttributes attr,
                        @RequestParam("editormd-image-file")MultipartFile file,
                        @RequestParam("dialog_id") String dialogId) throws Exception {
+
         Map map = new HashMap();
         if(!file.isEmpty()){
             try {
-              /*
-               * 这段代码执行完毕之后，图片上传到了工程的跟路径；
-               * 大家自己扩散下思维，如果我们想把图片上传到 d:/files大家是否能实现呢？
-               * 等等;
-               * 这里只是简单一个例子,请自行参考，融入到实际中可能需要大家自己做一些思考，比如：
-               * 1、文件路径；
-               * 2、文件名；
-               * 3、文件格式;
-               * 4、文件大小的限制;
-               */
                 System.out.println(file.getOriginalFilename());
                 BufferedOutputStream out = new BufferedOutputStream(new FileOutputStream(new File(file.getOriginalFilename())));
 //				out.write(file.getBytes());
@@ -59,6 +54,7 @@ public class UtilController {
             System.out.println("图片为空");
             //return"上传失败，因为文件是空的.";
         }
+
         return "redirect:/api/testMarkdownZHANG";
     }
 
@@ -67,7 +63,6 @@ public class UtilController {
                        HttpServletResponse response,
                        @RequestParam("dialog_id") String dialogId
             ) throws Exception {
-
         return  "api/testMarkdownZHANG";
     }
 
@@ -78,11 +73,56 @@ public class UtilController {
                          RedirectAttributes attr,
                         @RequestParam("dialog_id") String dialogId
     ) throws Exception {
+
         attr.addAttribute("dialog_id", dialogId);
         attr.addAttribute("message", "hello");
         attr.addAttribute("success", "1");
         attr.addAttribute("url", "/api/yuwei");
+
         return  "redirect:/api/testMarkdownZHANG";
+    }
+
+    private final StorageService storageService;
+
+    @Autowired
+    public FileUploadController(StorageService storageService) {
+        this.storageService = storageService;
+    }
+
+    @RequestMapping("/")
+    public String listUploadedFiles(Model model) throws IOException {
+
+        model.addAttribute("files", storageService.loadAll().map(
+                path -> MvcUriComponentsBuilder.fromMethodName(FileUploadController.class,
+                        "serveFile", path.getFileName().toString()).build().toString())
+                .collect(Collectors.toList()));
+
+        return "uploadForm";
+    }
+
+    @RequestMapping("/files/{filename:.+}")
+    @ResponseBody
+    public ResponseEntity<Resource> serveFile(@PathVariable String filename) {
+
+        Resource file = storageService.loadAsResource(filename);
+        return ResponseEntity.ok().header(HttpHeaders.CONTENT_DISPOSITION,
+                "attachment; filename=\"" + file.getFilename() + "\"").body(file);
+    }
+
+    @RequestMapping("/hhhhhhh")
+    public String handleFileUpload(@RequestParam("file") MultipartFile file,
+                                   RedirectAttributes redirectAttributes) {
+
+        storageService.store(file);
+        redirectAttributes.addFlashAttribute("message",
+                "You successfully uploaded " + file.getOriginalFilename() + "!");
+
+        return "redirect:/";
+    }
+
+    @ExceptionHandler(StorageFileNotFoundException.class)
+    public ResponseEntity<?> handleStorageFileNotFound(StorageFileNotFoundException exc) {
+        return ResponseEntity.notFound().build();
     }
 
 }
---
+++ src/main/java/com/potflesh/wenda/controller/SearchController.java
@@ -0,0 +1,7 @@
+package com.potflesh.wenda.controller;
+
+/**
+ * Created by bazinga on 17/04/2018.
+ */
+public class SearchController {
+}
