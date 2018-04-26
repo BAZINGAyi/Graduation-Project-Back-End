f39eefca289ea242cac0a88380eb69a25cb57146
zhangyuwei <570146800@qq.com>
2018-04-26 09:21:39 +0800

    : feat(feed): add feed api

--- src/main/java/com/potflesh/wenda/controller/FeedController.java
+++ src/main/java/com/potflesh/wenda/controller/FeedController.java
@@ -5,15 +5,19 @@ import com.potflesh.wenda.model.HostHolder;
 import com.potflesh.wenda.service.FeedService;
 import com.potflesh.wenda.service.FollowService;
 import com.potflesh.wenda.service.RedisService;
+import com.potflesh.wenda.utils.WendaUtil;
+import org.apache.commons.collections.map.HashedMap;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import com.potflesh.wenda.utils.RedisKeyUtil;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by bazinga on 2017/4/26.
@@ -54,6 +58,35 @@ public class FeedController {
 
         return "feeds";
     }
+
+    @RequestMapping(path = {"api/pullUserFeeds"},method = {RequestMethod.GET})
+    @ResponseBody
+    public String getUserPullFeeds(){
+
+        int localUserId = hostHolder.getUsers() == null ? 0 : hostHolder.getUsers().getId();
+        List<Integer> followees = new ArrayList<>();
+        Map<String, Object> maps = new HashedMap();
+        // 未登录
+        if (localUserId == 0){
+
+        }else {
+            // 查找我关注的人
+            followees = followService.getFollowees(localUserId, EntityType.ENTITY_USER,
+                    Integer.MAX_VALUE);
+            // 加上查找我关注的问题
+        }
+        // 未登录则获取最近所有用户的 feed
+        List<Feed> feeds = feedService.getUserFeeds(Integer.MAX_VALUE,followees,10);
+        maps.put("feeds",feeds);
+
+        if (feeds != null && feeds.size() != 0) {
+            return WendaUtil.getJSONString(200, maps);
+        } else {
+            return WendaUtil.getJSONString(400, maps);
+        }
+    }
+
+
 
     // 推的模式
     @RequestMapping(path = {"/pushfeeds"},method = {RequestMethod.GET})
